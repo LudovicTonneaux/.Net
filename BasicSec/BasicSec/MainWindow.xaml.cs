e56bb2ce31f2054dc178b1666a201bd4d0b8193f
LudovicTonneaux <ludovictonneaux@gmail.com>
2016-05-18 18:54:38 +0200

    Update

--- BasicSec/BasicSec/MainWindow.xaml.cs
+++ BasicSec/BasicSec/MainWindow.xaml.cs
@@ -3,7 +3,10 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -22,20 +25,33 @@ namespace BasicSec
     /// </summary>
     public partial class MainWindow : Window
     {
-        public string filepath; //pad van het bestand
-        public List<Persoon> personen = new List<Persoon>(); //lijst van alle zenders en ontvangers
+        //TODO
+        //String versturen naar server
+        //hashcheck
+        //layout
+        //IP koppelen aan namen -> txt bestand in hun map
+        //Personen kunnen Toevoegen
 
+
+        public List<string> personen = new List<string>(); //lijst van alle zenders en ontvangers
+        public TcpClient client = new TcpClient("192.168.1.1", 8888); //connecteren met de server
+        public NetworkStream stream;
+        public byte[] buf;
 
         public MainWindow()
         {
+            stream = client.GetStream();
+
             Personen();
 
             InitializeComponent();
+            
 
             listBoxZenders.ItemsSource = personen;
             listBoxZenders.SelectedIndex = 0;
             listBoxOntvangers.ItemsSource = personen;
             listBoxOntvangers.SelectedIndex = 1;
+
         }
 
         //RadioButtons
@@ -59,61 +75,13 @@ namespace BasicSec
         private void buttonBestand_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog openFileDialog1 = new OpenFileDialog();
-            openFileDialog1.Filter = "All files (*.*)|*.*";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt";
             openFileDialog1.Multiselect = false;
+            string filepath;
             if (openFileDialog1.ShowDialog() == true)
             {
                 filepath = openFileDialog1.FileName;
-                textBoxGekozenBestand.Text = filepath;
-            }
-        }
-
-        //Personen laden
-        private void Personen()
-        {
-            List<string> namen = new List<string>();
-            foreach (string s in Directory.GetDirectories(@".\Personen"))
-            {
-                namen.Add(s.Remove(0, 11));
-            }
-
-            foreach (string naam in namen)
-            {
-                Persoon persoon = new Persoon(naam);
-
-                if (!File.Exists(@".\Personen\" + naam + @"\Private.txt"))
-                {
-                    //private key aanmaken
-                    StreamReader reader = new StreamReader(@".\Personen\" + naam + @"\Private.txt");
-                    persoon.privateKey = reader.ReadLine();
-                    reader.Close();
-                    reader.Dispose();
-                }
-                else
-                {
-                    StreamReader reader = new StreamReader(@".\Personen\" + naam + @"\Private.txt");
-                    persoon.privateKey = reader.ReadLine();
-                    reader.Close();
-                    reader.Dispose();
-                }
-
-                if (!File.Exists(@".\Personen\" + naam + @"\Private.txt"))
-                {
-                    //public key aanmaken
-                    StreamReader reader = new StreamReader(@".\Personen\" + naam + @"\Public.txt");
-                    persoon.publicKey = reader.ReadLine();
-                    reader.Close();
-                    reader.Dispose();
-                }
-                else
-                {
-                    StreamReader reader = new StreamReader(@".\Personen\" + naam + @"\Public.txt");
-                    persoon.publicKey = reader.ReadLine();
-                    reader.Close();
-                    reader.Dispose();
-                }
-
-                personen.Add(persoon);
+                textBoxGekozenBestand.Text = File.ReadAllText(filepath);
             }
         }
 
@@ -122,12 +90,32 @@ namespace BasicSec
         {
             if (radioButtonDecrypteren.IsChecked == true)
             {
-                //Decrypteren
-                //HashCheck
+
+
             }
             else if (radioButtonEncrypteren.IsChecked == true)
             {
-                //Encrypteren
+                
+            }
+        }
+
+
+        public void NaarServer(string text)
+        {
+            buf = Encoding.UTF8.GetBytes(text + "\n");
+            stream.Write(buf, 0, text.Length + 1);
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            stream.Close();
+        }
+
+        public void Personen()
+        {
+            foreach (string naam in Directory.GetDirectories(@".\Personen"))
+            {
+                personen.Add(naam.Remove(0, 11));
             }
         }
     }
